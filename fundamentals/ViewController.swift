2511c102c17d447ec1c859a118f6c7d50ec4e2fc
Oluwaseun Erinle <oluwaseunerinle@Oluwaseuns-MacBook-Pro.local>
2017-02-14 20:10:45 -0500

    Initial Commit

---
+++ fundamentals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fundamentals
+//
+//  Created by Oluwaseun Erinle on 2/14/17.
+//  Copyright © 2017 megasure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
