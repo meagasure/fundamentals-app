841d74635f03d487d42485f6b4a4487b8c74ded0
S Erinle <erinle.oav@gmail.com>
2017-02-17 11:18:08 -0500

    Boolean fun

--- fundamentals/ViewController.swift
+++ fundamentals/ViewController.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var greetingLabel: UILabel!
-   
+    
     var tapCount = 0
     
     @IBAction func buttonPressed(_ sender: Any) {
@@ -23,10 +23,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var input_2: UITextField!
     
     @IBAction func secondPress(_ sender: Any) {
-//        tapCount = tapCount + 1
-//        if tapCount >= 5 {
-//            greetingLabel.text = "You tapped the button 5 times!"
-//        }
+        //        tapCount = tapCount + 1
+        //        if tapCount >= 5 {
+        //            greetingLabel.text = "You tapped the button 5 times!"
+        //        }
         
         //output is currently strings
         print(input_1.text!)
@@ -35,8 +35,16 @@ class ViewController: UIViewController {
         //Long way
         greetingLabel.text = String(Double(input_1.text!)! + Double(input_2.text!)!)
         
-        //Shorter way
-         greetingLabel.text = "Answer: \(Double(input_1.text!)! + Double(input_2.text!)!)"
+        //use the constant of 'let'
+        let addition = true
+        
+        if addition {
+            //Shorter way
+            greetingLabel.text = "Answer: \(Double(input_1.text!)! + Double(input_2.text!)!)"
+        } else {
+            greetingLabel.text = "Answer: \(Double(input_1.text!)! - Double(input_2.text!)!)"
+        }
+        
     }
     
     override func viewDidLoad() {
@@ -44,12 +52,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
